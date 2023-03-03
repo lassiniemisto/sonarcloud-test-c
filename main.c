40346400b69e620ec9908f8e72faaa055eeb2c34
Lassi Niemistö <lani@wapice.com>
2023-03-03 12:05:58 +0200

    Add bugs

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+int otherfunc(int a, int b)
+{
+    if (a > 4) return 6;
+}
+
+
 int main() {
    // printf() displays the string inside quotation
+
+
+   otherfunc(1, 2);
+
+   int * pointer = 0;
+   *pointer = 0xDEADBEEF;
    printf("Hello, World!");
    return 0;
 }
