efb2cad0c84f13a9e009a24d63b149b48fa49353
Lassi Niemistö <lani@wapice.com>
2023-03-03 12:10:41 +0200

    More difficult bug

--- main.c
+++ main.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
-int otherfunc(int a, int b)
+int * otherfunc(int a, int b)
 {
-    if (a > 4) return 6;
+    if (a > 4) return 0;
 }
 
 
 int main() {
    // printf() displays the string inside quotation
 
+   int * pointer = otherfunc(7, 2);
 
-   otherfunc(1, 2);
-
-   int * pointer = 0;
    *pointer = 0xDEADBEEF;
    printf("Hello, World!");
    return 0;
